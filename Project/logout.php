e852b19b01958e0c04cc43074c5575741a300ecb
kmr54 <kmr54@njit.edu>
2020-10-07 23:32:18 -0400

    updates

--- Project/logout.php
+++ Project/logout.php
@@ -1,12 +1,12 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-//starts/loads a session, basically tells php to do its magic
 session_start();
 // remove all session variables
 session_unset();
 // destroy the session
 session_destroy();
+?>
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
 echo "You're logged out (proof by dumping the session)<br>";
 echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 ?>
-<a href="home.php">Home</a>
--- Project/partials/index.php
+++ Project/partials/index.php
@@ -1,4 +1,6 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+header("Location: ../index.php");
+?>
 This is a placeholder index file for the project.
 Typically this would be a public homepage and I leave it up to the project requirements to fill this in.
 If we didn't have an index file navigating to the project folder would list our files.
