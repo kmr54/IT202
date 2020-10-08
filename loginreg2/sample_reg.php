f6bd896152caaf00a51164c983cfff87d5d06b5c
kmr54 <kmr54@njit.edu>
2020-10-07 23:33:37 -0400

    updates

--- loginreg2/sample_reg.php
+++ loginreg2/sample_reg.php
@@ -4,58 +4,55 @@
   <input type="email" id="email" name="email" required/>
   <label for="p1">Password:</label>
   <input type="password" id="p1" name="password" required/>
-  <input type="submit" name="login" value="Login"/>
+  <label for="p2">Confirm Password:</label>
+  <input type="password" id="p2" name="confirm" required/>
+  <input type="submit" name="register" value="Register"/>
 </form>
-
 <?php
-if(isset($_POST["login"])){
+if(isset($_POST["register"])){
   $email = null;
   $password = null;
+  $confirm = null;
   if(isset($_POST["email"])){
     $email = $_POST["email"];
   }
   if(isset($_POST["password"])){
     $password = $_POST["password"];
   }
+  if(isset($_POST["confirm"])){
+    $confirm = $_POST["confirm"];
+  }
   $isValid = true;
-  if(!isset($email) || !isset($password)){
+  //check if passwords match on the server side
+  if($password == $confirm){
+    echo "Passwords match <br>"; 
+  }
+  else{
+    echo "Passwords don't match<br>";
+    $isValid = false;
+  }
+  if(!isset($email) || !isset($password) || !isset($confirm)){
    $isValid = false; 
   }
   //TODO other validation as desired, remember this is the last line of defense
-  //here you'd probably want some email validation, for sake of example let's do a super basic one
-  if(!strpos($email, "@")){
-   $isValid = false;
-    echo "<br>Invalid email<br>";
-  }
   if($isValid){
+    $hash = password_hash($password, PASSWORD_BCRYPT);
     require_once("db.php");
     $db = getDB();
 	if(isset($db)){
 		//here we'll use placeholders to let PDO map and sanitize our data
-		$stmt = $db->prepare("SELECT email, password from Users WHERE email = :email LIMIT 1");
+		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
 		//here's the data map for the parameter to data
-		$params = array(":email"=>$email);
+		$params = array(":email"=>$email, ":password"=>$hash);
 		$r = $stmt->execute($params);
 		//let's just see what's returned
 		echo "db returned: " . var_export($r, true);
 		$e = $stmt->errorInfo();
-		if($e[0] != "00000"){
-			echo "uh oh something went wrong: " . var_export($e, true);
-		}
-		//since it's a select command we must fetch the results
-		//we'll tell pdo to give it to us as an associative array
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-		if($result && isset($result["password"])){
-			$password_hash_from_db = $result["password"];
-			if(password_verify($password, $password_hash_from_db)){
-			 echo "<br>Welcome! You're logged in!<br>"; 
-			}
-			else{
-			 echo "<br>Invalid password, get out!<br>"; 
-			}
+		if($e[0] == "00000"){
+			echo "<br>Welcome! You successfully registered, please login.";
 		}
 		else{
-			echo "<br>Invalid user<br>";
+			echo "uh oh something went wrong: " . var_export($e, true);
 		}
 	}
   }
