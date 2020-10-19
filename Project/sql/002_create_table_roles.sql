a9acb9b592c910f546d6e586f998dea89ee5403b
kmr54 <kmr54@njit.edu>
2020-10-19 12:04:18 -0400

    002

--- Project/sql/002_create_table_roles.sql
+++ Project/sql/002_create_table_roles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS  `Roles`
 (
-    `id`         int auto_increment not null,
+    `id`  int auto_increment not null,
     `name`      varchar(100)       not null unique,
     `description` varchar(100) default '',
     `is_active`  TINYINT(1) default 1,
@@ -8,4 +8,7 @@ CREATE TABLE IF NOT EXISTS  `Roles`
     `modified`   timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`id`)
 )
+
+
+
 
