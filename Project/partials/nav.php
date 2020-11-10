23a0b4e2920e6bf8d13f2667807cfa338a8d32e0
kmr54 <kmr54@njit.edu>
2020-11-10 00:40:40 -0500

    edited nav

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -4,15 +4,21 @@
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <nav>
-<ul class="nav">
-    <li><a href="home.php">Home</a></li>
-    <?php if (!is_logged_in()): ?>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-    <?php endif; ?>
-    <?php if (is_logged_in()): ?>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    <?php endif; ?>
-</ul>
+    <ul class="nav">
+        <li><a href="home.php">Home</a></li>
+        <?php if (!is_logged_in()): ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_account.php">Create Account</a></li>
+            <li><a href="test_list_account.php">View Accounts</a></li>
+            <li><a href="test_create_transactions.php">Create Transaction</a></li>
+            <li><a href="test_list_transactions.php">View Transaction</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()): ?>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
 </nav>
