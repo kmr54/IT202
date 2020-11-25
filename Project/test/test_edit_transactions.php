88a3cccdd194e7e4fbcdfff8e2d396f0d5fc3d28
kmr54 <kmr54@njit.edu>
2020-11-25 16:24:07 -0500

    fix

---
+++ Project/test/test_edit_transactions.php
@@ -0,0 +1,96 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+?>
+<?php
+//saving
+if (isset($_POST["save"])) {
+    //TODO add proper validation/checks
+    $amount = $_POST["amount"];
+    $source = $_POST["act_src_id"];
+    $dest = $_POST["act_dest_id"];
+    $type = $_POST["action_type"];
+    $memo = $_POST["memo"];
+    $db = getDB();
+    if (isset($id)) {
+        $stmt = $db->prepare("UPDATE Transactions set amount=:amount, act_src_id=:source, act_dest_id=:dest, action_type=:type, memo=:memo where id=:id");
+        $r = $stmt->execute([
+            ":amount" => $amount,
+            ":act_src_id" => $source,
+	    ":act_dest_id" => $dest,
+            ":action_type" => $type,
+            ":memo" => $memo,
+            ":id" => $id
+        ]);
+        if ($r) {
+            flash("Updated successfully with id: " . $id);
+        }
+        else {
+            $e = $stmt->errorInfo();
+            flash("Error updating: " . var_export($e, true));
+        }
+    else {
+        flash("ID isn't set, we need an ID in order to update");
+    }
+}
+?>
+
+<?php
+//fetching
+$result = [];
+if (isset($id)) {
+    $id = $_GET["id"];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT * FROM Transactions where id = :id");
+    $r = $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+$db = getDB();
+$stmt = $db->prepare("SELECT id,name from Accounts LIMIT 10");
+$r = $stmt->execute();
+$accounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+    <h3>Edit Transaction</h3>
+    <form method="POST">
+	<label>Amount</label>
+        <input type="number" name="amount" min="0" placeholder="0.00"/>
+        <label>Account Source</label>
+        <select name="act_src_id">
+            <?php foreach ($accounts as $account): ?>
+                <option value="<?php safer_echo($account["id"]); ?>"
+                ><?php safer_echo($account["account_number"]); ?></option>
+            <?php endforeach;?>
+        </select>
+        <label>Account Destination</label>
+        <select name="act_dest_id">
+            <?php foreach ($accounts as $account): ?>
+                <option value="<?php safer_echo($account["id"]); ?>"
+                ><?php safer_echo($account["account_number"]); ?></option>
+            <?php endforeach;?>
+        </select>
+        <label>Action Type</label>
+        <select name="action_type">
+            <option value="deposit">Deposit</option>
+            <option value="withdraw">Withdraw</option>
+            <option value="transfer">Transfer</option>
+        </select>
+        <label>Memo</label>
+        <input type="text" name="memo"/>
+        <input type="submit" name="save" value="Create"/>
+    </form>
+
+
+<?php require(__DIR__ . "/partials/flash.php");
+
